95d8fa7b9e2fbd876e28bfafb5e1b6b14a979cc8
Benjamin Mayrargue <benjamin@vapolia.fr>
2024-11-07 06:03:07 +0100

    add net8

--- src/Models/ContentDetent.cs
+++ src/Models/ContentDetent.cs
@@ -18,6 +18,12 @@ public partial class ContentDetent : Detent
         // HACK: from 'page.Window.Width' to 'page.Window?.Width ?? page.Width' 
         var r = page.Content.Measure(page.Window?.Width ?? page.Width - page.Padding.HorizontalThickness, maxSheetHeight);
 
-        return Math.Min(maxSheetHeight, r.Height + page.Padding.VerticalThickness);
+#if NET8_0
+        var height = r.Request.Height;
+#else
+        var height = r.Height;
+#endif
+        
+        return Math.Min(maxSheetHeight, height + page.Padding.VerticalThickness);
     }
 }
